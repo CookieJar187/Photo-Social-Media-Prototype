030de1e21aa79beda3b2530267fffbc312f1315e
CookieJar187 <benjamin.matanovic@gmail.com>
2026-06-29 19:05:52 +0200

    Integrated PostgreSQL to the project. The displayed data is now real data that exists in the database. Focus on enabling posting next.

--- backend/src/routes/profilesRoutes.js
+++ backend/src/routes/profilesRoutes.js
@@ -1,4 +1,5 @@
 import express from "express";
+import pool from "../db.js";
 import { getProfiles, getFollowers, getFollowing } from "../controllers/profilesController.js";
 import { requireAuth } from "../middleware/requireAuth.js";
 
@@ -7,5 +8,14 @@ const router = express.Router();
 router.get("/", requireAuth, getProfiles);
 router.get("/followers", requireAuth, getFollowers);
 router.get("/following", requireAuth, getFollowing);
+
+router.get("/test", async (req, res) => {
+
+    const result = await pool.query(
+        "SELECT * FROM users"
+    );
+
+    res.json(result.rows);
+});
 
 export default router;
